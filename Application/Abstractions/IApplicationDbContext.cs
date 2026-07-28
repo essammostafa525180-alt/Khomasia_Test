55a68b1fff41c4952cdc6e9076fa6bbe8935c72b
mostafa.twfeek <mostafa.twfeek@ahccinc.com>
2026-07-28 20:00:59 +0300

    auth changes

--- Application/Abstractions/IApplicationDbContext.cs
+++ Application/Abstractions/IApplicationDbContext.cs
@@ -13,29 +13,29 @@ namespace Application.Abstractions;
 public interface IApplicationDbContext
 {
     DatabaseFacade Database { get; }
-    public DbSet<Partation> Partations { get; set; }
-    public DbSet<HadithCollection> HadithCollections { get; set; }
-    public DbSet<NarratorStudent> NarratorStudents { get; set; }
-    public DbSet<NarratorsCriticism> NarratorsCriticisms { get; set; }
-    public DbSet<Classification> Classifications { get; set; }
-    public DbSet<Book> Books { get; set; }
-    public DbSet<Bab> Babs { get; set; }
-    public DbSet<Hadith> Hadiths { get; set; }
-    public DbSet<HadithTakhreejMessing> HadithTakhreejMessings { get; set; }
-    public DbSet<HadithSharh> HadithSharhs { get; set; }
-    public DbSet<HadithTakhreej> HadithTakhreejs { get; set; }
-    public DbSet<SharhBook> SharhBooks { get; set; }
-    public DbSet<Narrator> Narrators { get; set; }
-    public DbSet<NarratorTeacher> NarratorTeachers { get; set; }
-    public DbSet<HadithLanguages> HadithLanguages { get; set; }
-    public DbSet<HadithTranslations> HadithTranslations { get; set; }
-    public DbSet<HadithTranslationsMissing> HadithTranslationsMissings { get; set; }
-    public DbSet<HadithMissing> HadithMissings { get; set; }
-    //هيتشال لم الداتا تتصلح
-    public DbSet<HadithSharhMissing> hadithSharhMissings { get; set; }
-    public DbSet<HadithNarrator> HadithNarrators { get; set; }
+    //public DbSet<Partation> Partations { get; set; }
+    //public DbSet<HadithCollection> HadithCollections { get; set; }
+    //public DbSet<NarratorStudent> NarratorStudents { get; set; }
+    //public DbSet<NarratorsCriticism> NarratorsCriticisms { get; set; }
+    //public DbSet<Classification> Classifications { get; set; }
+    //public DbSet<Book> Books { get; set; }
+    //public DbSet<Bab> Babs { get; set; }
+    //public DbSet<Hadith> Hadiths { get; set; }
+    //public DbSet<HadithTakhreejMessing> HadithTakhreejMessings { get; set; }
+    //public DbSet<HadithSharh> HadithSharhs { get; set; }
+    //public DbSet<HadithTakhreej> HadithTakhreejs { get; set; }
+    //public DbSet<SharhBook> SharhBooks { get; set; }
+    //public DbSet<Narrator> Narrators { get; set; }
+    //public DbSet<NarratorTeacher> NarratorTeachers { get; set; }
+    //public DbSet<HadithLanguages> HadithLanguages { get; set; }
+    //public DbSet<HadithTranslations> HadithTranslations { get; set; }
+    //public DbSet<HadithTranslationsMissing> HadithTranslationsMissings { get; set; }
+    //public DbSet<HadithMissing> HadithMissings { get; set; }
+    ////هيتشال لم الداتا تتصلح
+    //public DbSet<HadithSharhMissing> hadithSharhMissings { get; set; }
+    //public DbSet<HadithNarrator> HadithNarrators { get; set; }
 
-    public DbSet<ContactMessage> ContactMessages { get; set; }
+    //public DbSet<ContactMessage> ContactMessages { get; set; }
 
 
 
---
+++ Domain/Common/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
